013877705f378a5570a7034fc40966359957af9d
aggeev <den.ageev.2014@mail.ru>
2022-07-22 15:24:36 +0300

    ex1

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿int  number,number2,number3;
+number = new Random().Next(100,999);
+Console.WriteLine(" число "+number);
+number2 = (number/10) % 10;
+number3 = (number+10)%10;
+Console.WriteLine(" второе число "+ number2);
+Console.WriteLine(" третье число "+ number3);
+
